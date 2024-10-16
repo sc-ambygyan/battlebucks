b68ad95c9e37590d10f05147172135b4dcb0be26
sc-ambygyan <155875815+sc-ambygyan@users.noreply.github.com>
2024-10-16 09:59:01 +0530

    Initial Commit

---
+++ battlebucks/battlebucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  battlebucksApp.swift
+//  battlebucks
+//
+//  Created by Ambarish Gyanendra on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct battlebucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
